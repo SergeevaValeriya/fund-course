bd76e2997081a88a10fe4a0b16f65cd61ffd0cd4
Valeriya <vvsergeeva@inbox.ru>
2021-10-18 21:22:04 +0300

    Added search and sorting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, { useState } from "react";
+import React, {useMemo, useState} from "react";
 import "./styles/App.css"
 import PostsList from "./components/PostsList";
 import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: "JS", body: "Description"},
-        {id: 2, title: "JS 2", body: "Description"},
-        {id: 3, title: "JS 3", body: "Description"}
+        {id: 1, title: "DIY", body: "Coolest description"},
+        {id: 2, title: "Avengers", body: "Strange Description"},
+        {id: 3, title: "Bazinga", body: "Great Description"}
     ]);
+    const [filter, setFilter] = useState({sort: '', query: ''});
+
+    const sortedPosts = useMemo(() => {
+        console.log('Sorted posts function worked');
+        if(filter.sort) {
+            return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]));
+        }
+        return posts;
+    }, [filter.sort, posts]);
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    }, [filter.query, sortedPosts]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+
     return (
     <div className="App">
         <PostForm create={createPost} />
-        <PostsList posts={posts} title="Posts List 1" />
-
+        <hr style={{margin: '15px 0'}} />
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+        <PostsList remove={removePost} posts={sortedAndSearchedPosts} title="Posts List 1" />
     </div>
     );
 };
