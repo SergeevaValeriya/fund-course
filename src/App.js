d5a28bc7808611f586d1134e0d95319e35804b84
Valeriya <vvsergeeva@inbox.ru>
2021-10-19 19:40:42 +0300

    Work with API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, {useMemo, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { usePosts } from "./hooks/usePosts";
 import "./styles/App.css"
 import PostsList from "./components/PostsList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
-import {usePosts, useSortedPosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: "DIY", body: "Coolest description"},
-        {id: 2, title: "Avengers", body: "Strange Description"},
-        {id: 3, title: "Bazinga", body: "Great Description"}
-    ]);
+    const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+    useEffect(() => {
+        fetchPosts()
+    }, []);
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false);
+    }
+
+    async function fetchPosts() {
+        const posts = await PostService.getAll();
+        setPosts(posts)
     }
 
     const removePost = (post) => {
