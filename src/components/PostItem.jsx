b34867adf5429998caaf0e257d59b09fc5934c5a
Valeriya <vvsergeeva@inbox.ru>
2021-10-21 22:31:36 +0300

    Added single post page and comments

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import MyButton from "./UI/button/MyButton";
+import { useHistory } from "react-router-dom";
 
 const PostItem = (props) => {
+    const router = useHistory();
+    console.log(router);
     return (
         <div className="post">
             <div className="post__content">
@@ -11,7 +14,12 @@ const PostItem = (props) => {
                 </div>
             </div>
             <div className="post__btns">
-                <MyButton onClick={() => props.remove(props.post)} >Delete</MyButton>
+                <MyButton onClick={() => router.push(`/posts/${props.post.id}`)}>
+                    Open
+                </MyButton>
+                <MyButton onClick={() => props.remove(props.post)}>
+                    Delete
+                </MyButton>
             </div>
         </div>
     );
