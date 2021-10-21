1979f137164195267b9619c14793fafabb4f5744
Valeriya <vvsergeeva@inbox.ru>
2021-10-21 23:30:02 +0300

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./styles/App.css"
-import { BrowserRouter, Link, Route, Switch, Redirect } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/UI/navbar/Navbar";
-import Error from "./pages/Error";
 import AppRouter from "./components/AppRouter";
+import {AuthContext} from "./context";
 
 
 function App() {
+    const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+        if(localStorage.getItem('auth')) {
+            setIsAuth(true)
+        }
+    }, [])
+
     return (
-        <BrowserRouter>
-            <Navbar />
-            <AppRouter />
-        </BrowserRouter>
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth
+        }}>
+            <BrowserRouter>
+                <Navbar />
+                <AppRouter />
+            </BrowserRouter>
+        </AuthContext.Provider>
     )
 };
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
+import { publicRoutes, privateRoutes } from "../router/routes";
+import {AuthContext} from "../context";
 
 
 const AppRouter = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext);
+    console.log(isAuth)
     return (
-        <Switch>
-            <Route path="/about">
-                <About />
-            </Route>
-            <Route exact path="/posts">
-                <Posts />
-            </Route>
-            <Route exact path="/posts/:id">
-                <PostIdPage />
-            </Route>
-            <Route path="/error">
-                <Error />
-            </Route>
-            <Redirect to="/posts"/>
-        </Switch>
+        isAuth
+        ? <Switch>
+                {privateRoutes.map(route =>
+                    <Route
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                        key={route.path}
+                    />
+                )}
+                <Redirect to="/posts"/>
+            </Switch>
+        :
+            <Switch>
+                {publicRoutes.map(route =>
+                    <Route
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                        key={route.path}
+                    />
+                )}
+                <Redirect to="/login"/>
+            </Switch>
+
     );
 };
 
